c364cc7c8d8dc7efaeb02edc91f6da374e15b27b
gagansingh894 <gds31.gagandeepsingh@gmail.com>
2022-01-13 02:30:03 +0530

    driver code

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gagansingh894/go-basic-rpc-server/client"
+	"github.com/gagansingh894/go-basic-rpc-server/server"
+	"log"
+	"net"
+	"net/http"
+	"net/rpc"
+	"time"
+)
+
+func main() {
+	go func() {
+		time.Sleep(5)
+		fmt.Println("Running client test")
+		client.TestClient()
+	}()
+
+	api := new(server.API)
+	err := rpc.Register(api)
+	if err != nil {
+		log.Fatal("error registering API", err)
+	}
+	rpc.HandleHTTP()
+	listener, err := net.Listen("tcp", ":4040")
+	if err != nil {
+		log.Fatal("Listener error", err)
+	}
+	log.Printf("serving rpc on port %d", 4040)
+	err = http.Serve(listener, nil)
+
+	if err != nil {
+		log.Fatal("error serving: ", err)
+	}
+
+}
